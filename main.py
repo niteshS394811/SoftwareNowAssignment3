14dccdd92195d547835bba2d1f734cc307d779cc
Nitesh Raj Chaudhary <s394811@students.cdu.edu.au>
2025-10-02 22:29:32 +0930

    final check and added group details in main file

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Assignment 3
+Group Name: [Group No. 11]
+Group Members:
+[Bidhan Chaudhary] - [S394807]
+[Dipesh Sedhai] - [S395457]
+[Nitesh Raj Chaudhary] - [S394811]
+[Shova Bhujel] - [S396449]
+
+"""
 import tkinter as tk
 from gui.app import AIModelGUI
 
